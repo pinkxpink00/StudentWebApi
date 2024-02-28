67ab086dbf06ee23b6f6880c227de24e0be5dd17
pinkxpink <kyperu07@gmail.com>
2024-02-28 23:30:28 +0200

    add repo

---
+++ StudentWebApi/Repository/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using StudentWebApi.Models;
+
+namespace StudentWebApi.Repository
+{
+    public interface IStudentRepository
+    {
+        Task<IEnumerable<Student>> Search (string name,Gender? gender);
+        Task<Student> GetStudend(int id);
+        Task<IEnumerable<Student>> GetStudents();
+        Task<Student> GetStudentByEmail (string email);
+        Task<Student> AddStudent (Student student);
+        Task<Student> UpdateStudent (Student student);
+        Task<Student> DeleteStudent (int id);
+    }
+}
